0b45044b67716e8f918bd62820c1559ff73949a3
Victoria Frolova <morriface@mail.ru>
2021-10-19 17:31:46 +0300

    VF first commit: task done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { StatsProvider } from '../src/app-context/app-context';
+import CreateDevColumn from '../src/app-utils/app-utils';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StatsProvider>
+      <CreateDevColumn/>
+
+      {/* <div className="App">
+        <div className="wrapper">
+          <h1 className="wrapper__header">Количество пройденных тестов "OS Doors"</h1>
+          <div className="wrapper__test-columns">
+            <div className="test-column dev">
+              <figure>
+                <div className="test-column__client value">50</div>
+                <div className="test-column__server value">20</div>
+                <div className="test-column__data value">70</div>
+                <figcaption>dev</figcaption>
+              </figure>
+            </div>
+            <div className="test-column test">
+            <span className="arrow"></span>
+              <figure>
+                <div className="test-column__client value"></div>
+                <div className="test-column__server value"></div>
+                <div className="test-column__data value"></div>
+                <figcaption>test</figcaption>
+              </figure>
+            </div>
+            <div className="test-column prod">
+              <span className="arrow"></span>
+              <figure>
+                <div className="test-column__client value"></div>
+                <div className="test-column__server value"></div>
+                <div className="test-column__data value">70</div>
+                <figcaption>prod</figcaption>
+              </figure>
+            </div>
+            <div className="test-column norm">
+              <figure>
+                <div className="test-column__norm"></div>
+                <figcaption>норматив</figcaption>
+              </figure>
+            </div>
+          </div>
+          <div className="wrapper__test-specifications">
+            <div className="test-specification test-specification__client"><span></span>Клиентская часть</div>
+            <div className="test-specification test-specification__server"><span></span>Серверная часть</div>
+            <div className="test-specification test-specification__data"><span></span>База данных</div>
+          </div>
+        </div>
+      </div> */}
+    </StatsProvider>
   );
 }
 
